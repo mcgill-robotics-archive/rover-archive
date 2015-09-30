e7b4a031ade48dd4c13c9c2f45b5fe677221ea3c
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2015-09-29 21:24:54 -0400

    Comment the interface of the ahrs library
    Comment the ahrs library properly for doxygen

--- catkin_ws/src/ahrs/libraries/src/AhrsVirtual.h
+++ catkin_ws/src/ahrs/libraries/src/AhrsVirtual.h
@@ -18,12 +18,35 @@ namespace lineranger
 {
 namespace ahrs
 {
-
+/**
+ * \brief Implementation of the Ahrs interface with random data generation instead of physical device.
+ *
+ * This class add the option to generate virtual devices that provide dummy data to test the system from start to end.
+ */
 class AhrsVirtual: public Ahrs {
 public:
+
+    /**
+     * \brief Constructor starts execution creation of random statuses
+     *
+     * Creates a periodic thread to start parallel call to the status updates.
+     */
     AhrsVirtual();
+
+    /**
+     * \brief Destructor.
+     */
     virtual ~AhrsVirtual();
 
+    /**
+     * \brief Realisation of the interface
+     *
+     * Implementation of the get status method.
+     *
+     * Return the latest information in a thread safe manner.
+     *
+     * \return Structure containing the most up-to-date position and orientation information received from the device.
+     */
     virtual AhrsStatus getStatus();
 
 private:
@@ -35,7 +58,11 @@ private:
     /// Controller periodic thread
     boost::scoped_ptr<common::PeriodicThread> mpThread;
 
-    /// Method to be called by internal periodic thread
+    /**
+     * \brief Update the internal structure with some random values
+     *
+     * Method to be called by internal periodic thread
+     */
     void update();
 
 
