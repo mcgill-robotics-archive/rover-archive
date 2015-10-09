05eba97fde0d94ba8e2d7598c2377d6054bc1e41
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2015-10-08 22:02:59 -0400

    Test ahrs factory for proper instantiation

--- catkin_ws/src/ahrs/test/ahrs_unit_test.cpp
+++ catkin_ws/src/ahrs/test/ahrs_unit_test.cpp
@@ -1,5 +1,19 @@
 #include <gtest/gtest.h>
-#include "../libraries/include/ahrs/AhrsConfig.h"
+#include "../libraries/include/ahrs/Ahrs.h"
+#include <boost/smart_ptr/scoped_ptr.hpp>
+#include <stdexcept>
+
+
+
+class AhrsTest: public ::testing::Test
+{
+    protected:
+        AhrsTest(){}
+        virtual ~AhrsTest(){}
+
+        virtual void SetUp(){}
+        virtual void TearDown () {}
+};
 
 TEST(AhrsTest, defaultConfigTest)
 {
@@ -8,6 +22,23 @@ TEST(AhrsTest, defaultConfigTest)
     ASSERT_EQ(115200, ahrsConfig.getBaudRate());
     ASSERT_EQ("/dev/ahrs", ahrsConfig.getDeviceName());
     ASSERT_TRUE(ahrsConfig.isSimulation());
+}
+
+TEST(AhrsTest, factoryTest)
+{
+    try{
+        lineranger::ahrs::AhrsConfig ahrsConfig;
+        ahrsConfig.setSimulation(true);
+
+        boost::scoped_ptr<lineranger::ahrs::Ahrs> ahrs
+            (lineranger::ahrs::Ahrs::createAhrs(ahrsConfig));
+        lineranger::ahrs::AhrsStatus ahrsStatus = ahrs->getStatus();
+    }
+    catch (std::runtime_error& error)
+    {
+        ASSERT_TRUE(false) <<
+            "an exception was thrown trying to create a virtual ahrs"; 
+    }
 }
 
 int main (int argc, char ** argv)
