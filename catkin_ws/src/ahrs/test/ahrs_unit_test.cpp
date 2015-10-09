60da52edd0299f9ae101b7fce9424c39e1c15820
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2015-10-08 21:20:57 -0400

    Create additionnal tests for ahrs

--- catkin_ws/src/ahrs/test/ahrs_unit_test.cpp
+++ catkin_ws/src/ahrs/test/ahrs_unit_test.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 #include "../libraries/include/ahrs/AhrsConfig.h"
 
-TEST(AhrsTest, configTest)
+TEST(AhrsTest, defaultConfigTest)
 {
     lineranger::ahrs::AhrsConfig ahrsConfig;
 
     ASSERT_EQ(115200, ahrsConfig.getBaudRate());
+    ASSERT_EQ("/dev/ahrs", ahrsConfig.getDeviceName());
+    ASSERT_TRUE(ahrsConfig.isSimulation());
 }
 
 int main (int argc, char ** argv)
