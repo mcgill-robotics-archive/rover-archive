b07d3594aa63f2b280e75031063e4f8e4ac1cf73
Lavoie-Boutin <DD8641@ireq.ca>
2016-05-17 16:02:27 -0400

    Convert arduino reset to object and add GPIO calls

--- catkin_ws/src/arduino/scripts/reset_arduino.py
+++ catkin_ws/src/arduino/scripts/reset_arduino.py
@@ -2,22 +2,37 @@
 
 from arduino.srv import *
 import rospy
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error, could not import the GPIO library")
+    exit(1)
 
-def service_callback(request):
-    rospy.loginfo("[Arduino Reset] : Resetting the arduino")
+class ResetArduinoServer(object):
+    def __init__(self, output_pin):
+        #todo: change channel to proper pin (maybe a ros param)
+        self.channel = 7        
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.channel, GPIO.OUT, initial=GPIO.LOW)
+        s = rospy.Service('reset_arduino_srv', ResetArduino, service_callback)
+        rospy.loginfo("[Arduino Reset] : Server ready to process requests")
+        rospy.spin()
+    
+    def service_callback(self, request):
+        rospy.loginfo("[Arduino Reset] : Resetting the arduino")
 
-    #pull pin high#
-    rospy.Rate(10).sleep()
-    #pull pin low again#
+        #pull pin high#
+        GPIO.output(self.channel, GPIO.HIGH)
+        rospy.Rate(10).sleep()
+        #pull pin low again#
+        GPIO.output(self.channel, GPIO.LOW)
 
-    return ResetArduinoResponse(True)
+        return ResetArduinoResponse(True)
 
-
-def reset_arduino_server():
-    rospy.init_node('reset_arduino')
-    s = rospy.Service('reset_arduino_srv', ResetArduino, service_callback)
-    rospy.loginfo("[Arduino Reset] : Server ready to process requests")
-    rospy.spin()
+    def __del__(self):
+        GPIO.cleanup()
 
 if __name__ == '__main__':
-    reset_arduino_server()
+    rospy.init_node('reset_arduino')
+    ResetArduinoServer()
+
