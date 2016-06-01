312a07da835a301f2e2344377842fc8d3d401bf8
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2016-06-01 14:45:12 -0400

    Finalized pin maping for science

--- catkin_ws/src/arduino/src/science/science.cpp
+++ catkin_ws/src/arduino/src/science/science.cpp
@@ -7,27 +7,36 @@
 #include "Barometer.h"
 #include "PhSensor.h"
 #include <arduino/sensor.h>
+#include <arduino/LimitSwitchScience.h>
 #include "Sensors.h"
 #include <std_msgs/Int16.h>
 #include "pins_auger.h"
 #include "Servo.h"
+
+#define SWITCH_PUB_TIMEOUT 500
+
+
+unsigned long switch_pub_schedule = 0;
 void sensorServiceCallback(const arduino::sensor::Request &request, arduino::sensor::Response &response);
 void handle_auger_velocity(const std_msgs::Int16 & message);
 void handle_auger_position(const std_msgs::Int16 & message);
 void handle_gate_position(const std_msgs::Int16 & message);
 void handle_temp_prob_position(const std_msgs::Int16 & message);
 
-//Servo tempProbServo;
+Servo tempProbServo;
+
+arduino::LimitSwitchScience limitSwitchMsg;
 
 int16_t last_command_auger_velocity = 0;
 int16_t last_command_auger_position = 0;
 int16_t last_command_gate_position = 0;
 int16_t last_command_prob_position = 0;
 
-ros::Subscriber<std_msgs::Int16> augerVelocitySub("~auger_velocity",&handle_auger_velocity);
-ros::Subscriber<std_msgs::Int16> augerPositionSub("~auger_position",&handle_auger_position);
-ros::Subscriber<std_msgs::Int16> gatePositionSub("~gate_position",&handle_gate_position);
-ros::Subscriber<std_msgs::Int16> tempeProbPositionSub("~temp_prob_position",&handle_temp_prob_position);
+ros::Subscriber<std_msgs::Int16> augerVelocitySub("~auger_velocity", &handle_auger_velocity);
+ros::Subscriber<std_msgs::Int16> augerPositionSub("~auger_position", &handle_auger_position);
+ros::Subscriber<std_msgs::Int16> gatePositionSub("~gate_position", &handle_gate_position);
+ros::Subscriber<std_msgs::Int16> tempeProbPositionSub("~temp_prob_position", &handle_temp_prob_position);
+ros::Publisher limitSwitchPub("~limit_switch", &limitSwitchMsg);
 
 Barometer * barometer;
 PhSensor * phSensor;
@@ -54,13 +63,14 @@ void setup()
     pinMode(PIN_LIMIT_SWITCH_PROB, INPUT_PULLUP);
     pinMode(PIN_LIMIT_SWITCH_GATE, INPUT_PULLUP);
     
-    //tempProbServo.attach(PIN_TEMP_PROB_POSITION);
+    tempProbServo.attach(PIN_TEMP_PROB_POSITION);
     nodeHandle.initNode();
     nodeHandle.advertiseService(sensorService);
     nodeHandle.subscribe(augerVelocitySub);
     nodeHandle.subscribe(augerPositionSub);
     nodeHandle.subscribe(gatePositionSub);
     nodeHandle.subscribe(tempeProbPositionSub);
+    nodeHandle.advertise(limitSwitchPub);
     //things which have to be in the setup
     //barometer
     barometer = new Barometer;
@@ -73,18 +83,26 @@ void setup()
 void loop ()
 {
     nodeHandle.spinOnce();
-    delay(1);
     if (!digitalRead(PIN_LIMIT_SWITCH_UP) && last_command_auger_position > 0){
         analogWrite(PIN_AUGER_POSITION_PWM, 0);
     }
     if (!digitalRead(PIN_LIMIT_SWITCH_DOWN) && last_command_auger_position < 0) {
         analogWrite(PIN_AUGER_POSITION_PWM, 0);
     }
-    //if (!digitalRead(PIN_LIMIT_SWITCH_GATE) && last_command_gate_position > 0) {
-    //    analogWrite(PIN_GATE_POSITION_PWM, 0);
-    //}
+    if (!digitalRead(PIN_LIMIT_SWITCH_GATE) && last_command_gate_position > 0) {
+        analogWrite(PIN_GATE_POSITION_PWM, 0);
+    }
     if (!digitalRead(PIN_LIMIT_SWITCH_PROB) && last_command_prob_position > 0) {
-        //tempProbServo.writeMicroseconds(1500);
+        tempProbServo.writeMicroseconds(1500);
+    }
+
+    if( switch_pub_schedule < millis() ){
+        switch_pub_schedule += SWITCH_PUB_TIMEOUT;
+        limitSwitchMsg.limit_switch_up = digitalRead(PIN_LIMIT_SWITCH_UP);
+        limitSwitchMsg.limit_switch_down = digitalRead(PIN_LIMIT_SWITCH_DOWN);
+        limitSwitchMsg.limit_switch_prob = digitalRead(PIN_LIMIT_SWITCH_PROB);
+        limitSwitchMsg.limit_switch_gate = digitalRead(PIN_LIMIT_SWITCH_GATE);
+        limitSwitchPub.publish(&limitSwitchMsg);
     }
 }
 
@@ -130,27 +148,27 @@ void handle_auger_position(const std_msgs::Int16 & message){
 
 void handle_gate_position(const std_msgs::Int16 & message){
     last_command_gate_position = message.data;
-    if(digitalRead(PIN_LIMIT_SWITCH_GATE) && last_command_gate_position > 0 ){
+    if(digitalRead(PIN_LIMIT_SWITCH_UP) && last_command_gate_position > 0 ){
         digitalWrite(PIN_GATE_POSITION_INA, HIGH);
         digitalWrite(PIN_GATE_POSITION_INB, LOW);
-        analogWrite(PIN_GATE_POSITION_PWM, min(last_command_gate_position,255));
-    } else if (digitalRead(PIN_LIMIT_SWITCH_DOWN) && last_command_gate_position < 0){
+        analogWrite(PIN_GATE_POSITION_PWM, min(last_command_gate_position,120));
+    } else if (last_command_gate_position < 0){
         digitalWrite(PIN_GATE_POSITION_INA, LOW);
         digitalWrite(PIN_GATE_POSITION_INB, HIGH);
-        analogWrite(PIN_GATE_POSITION_PWM, min(abs(last_command_gate_position),255));  
+        analogWrite(PIN_GATE_POSITION_PWM, min(abs(last_command_gate_position),120));  
     } else { 
-        analogWrite(PIN_AUGER_POSITION_PWM, 0);  
+        analogWrite(PIN_GATE_POSITION_PWM, 0);  
     }
 }
 
 void handle_temp_prob_position(const std_msgs::Int16 & message){
     last_command_prob_position = message.data;
     if(digitalRead(PIN_LIMIT_SWITCH_PROB) && last_command_prob_position > 0 ){
-        //tempProbServo.writeMicroseconds(1500 + last_command_prob_position);
+        tempProbServo.writeMicroseconds(1500 + last_command_prob_position);
     } else if (last_command_prob_position < 0){
-        //tempProbServo.writeMicroseconds(1500 + last_command_prob_position);
+        tempProbServo.writeMicroseconds(1500 + last_command_prob_position);
     } else { 
-        //tempProbServo.writeMicroseconds(1500);
+        tempProbServo.writeMicroseconds(1500);
     }
 
 }
