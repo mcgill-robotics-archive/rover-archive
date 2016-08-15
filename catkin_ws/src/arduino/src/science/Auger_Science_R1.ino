517f54127c8b6c9ebee39b632557c2a11b435d68
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-08-15 18:04:47 -0400

    Add todos and comments

--- catkin_ws/src/arduino/src/science/Auger_Science_R1.ino
+++ catkin_ws/src/arduino/src/science/Auger_Science_R1.ino
@@ -1,5 +1,5 @@
 #include "HX711.h"
-#include <Servo.h> 
+#include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_MPL3115A2.h>
 
@@ -11,49 +11,59 @@ int InA1 = 26, InB1 = 27, PWM1 = 2, InA2 = 36, InB2 = 37, PWM2 = 3;
 
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 Servo auger_servo, rock_servo, soil_servo;
-String readString;
+String readString = "";
 int limit_switch = 0, wind_sensor_value = 0;
-float wind_sensor_voltage = 0, wind_speed = 0, voltage_constant = .004882814, voltage_min = .4, voltage_max = 2.0, wind_speed_max = 32;
+float wind_sensor_voltage = 0, wind_speed = 0, voltage_constant = .004882814,
+voltage_min = .4, voltage_max = 2.0, wind_speed_max = 32;
 
 void setup() {
   Serial.begin(38400);
-  
+
+
+  // initial closed state
   auger_servo.write(0);
   rock_servo.write(0);
   soil_servo.write(0);
-  
+
+  // Setup pins
   auger_servo.attach(7);
   rock_servo.attach(9);
   soil_servo.attach(11);
-  
+
   pinMode(InA1, OUTPUT);
   pinMode(InB1, OUTPUT);
   pinMode(PWM1, OUTPUT);
-  
+
   pinMode(InA2, OUTPUT);
   pinMode(InB2, OUTPUT);
   pinMode(PWM2, OUTPUT);
-  
+
+  // Limit switches
   pinMode(8, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
-  
-  auger_loadcell.set_scale(2280.f);                   
-  auger_loadcell.tare();				       
-  top_loadcell.set_scale(2280.f);                     
-  top_loadcell.tare();
-  				 
+
+  // We need to add this functionality
+  auger_loadcell.set_scale(2280.f);
+  auger_loadcell.tare();
+  top_loadcell.set_scale(2280.f);
+  top_loadcell.tare(); // We want the driver to be able to do this when he wants
+
+
   auger_loadcell.read();
   top_loadcell.read();
-  
-  Serial.println("Ready");
+
+  Serial.println("Ready");  // Replace with ROS log
 }
 
-void loop() { 
+void loop() {
+  // Dealt with
   if (! baro.begin()) {
     Serial.println("Couldnt find sensor");
     return;
   }
+
+  // The limitations verified by limit switches - Dealt with
   int topLimit = digitalRead(8), bottomLimit = digitalRead(12);
   if (limit_switch == 2 && bottomLimit == LOW){
     analogWrite(PWM2, 0);
@@ -65,31 +75,33 @@ void loop() {
     limit_switch = 0;
     Serial.println("top limit switch");
   }
+
+
   if (Serial.available()) {
     char c = Serial.read();
     if (c == ',') {
       if (readString.length() >0) {
         int n = readString.toInt();
-        //************************************************************************************Zero loadcells        
+        //************************************************************************************Zero loadcells
         if(readString.indexOf('z') >0) {
-          auger_loadcell.tare();				                           
+          auger_loadcell.tare();
           top_loadcell.tare();
           Serial.println("Zero");}
-        //*****************************************************************************Rock and soil loadcell        
+        //*****************************************************************************Rock and soil loadcell
         if(readString.indexOf('t') >0) {
           Serial.print("Rock and soil loadcell:\t");
           float load_t = ((abs(top_loadcell.get_units(10)))*5.0552);
           Serial.println(load_t, 2);}
-        //*************************************************************************************Auger loadcell        
+        //*************************************************************************************Auger loadcell
         if(readString.indexOf('b') >0) {
           Serial.print("Auger loadcell:\t");
           Serial.println(auger_loadcell.get_units(10), 2);}
-        //*******************************************************************************************Humidity        
+        //*******************************************************************************************Humidity
         if(readString.indexOf('h') >0) {
           int humidity_value = analogRead(humidity_sensor_pin);
           Serial.print("Humidity Level (0-1023): ");
           Serial.println(1023-humidity_value);}
-        //*****************************************************************************************Wind speed        
+        //*****************************************************************************************Wind speed
         if(readString.indexOf('w') >0) {
           wind_sensor_value = analogRead(wind_sensor_pin);
           wind_sensor_voltage = wind_sensor_value * voltage_constant;
@@ -102,18 +114,18 @@ void loop() {
           Serial.print("Wind speed: ");
           Serial.print(wind_speed);
           Serial.println("m/s");}
-        //******************************************************************************************Pressure        
+        //******************************************************************************************Pressure
         if(readString.indexOf('p') >0) {
           float pascals = ((baro.getPressure()/1000) + 100);
-          Serial.print(pascals); 
+          Serial.print(pascals);
           Serial.println(" kPa");
           float altm = baro.getAltitude();
-          Serial.print(altm); 
+          Serial.print(altm);
           Serial.println(" m");
           float tempC = baro.getTemperature();
-          Serial.print(tempC); 
+          Serial.print(tempC);
           Serial.println(" C");}
-        //**************************************************************************************************        
+        //**************************************************************************************************
         if(readString.indexOf('a') >0) {
           if (n == 1) {
             Serial.println("sealing auger soil sample box");
@@ -129,7 +141,7 @@ void loop() {
             auger_servo.write(n);
           }
         }
-        //**************************************************************************************************        
+        //**************************************************************************************************
         if(readString.indexOf('r') >0) {
           if (n == 1) {
             Serial.println("sealing rock sample box");
@@ -145,7 +157,7 @@ void loop() {
             rock_servo.write(n);
           }
         }
-        //**************************************************************************************************        
+        //**************************************************************************************************
         if(readString.indexOf('s') >0) {
           if (n == 1) {
             Serial.println("sealing soil sample box");
@@ -161,13 +173,13 @@ void loop() {
             soil_servo.write(n);
           }
         }
-        //**************************************************************************************************        
+        //**************************************************************************************************
         if(readString.indexOf('k') >0) {
           analogWrite(PWM1, 0);
           analogWrite(PWM2, 0);
           limit_switch = 0;
           Serial.println("kill auger and carage");}
-        //**************************************************************************************************        
+        //**************************************************************************************************
         if(readString.indexOf('d') >0) {
           if(n < 0) {
             digitalWrite(InB1, HIGH);
@@ -188,7 +200,7 @@ void loop() {
             Serial.println("drill stop");
           }
         }
-        //**************************************************************************************************        
+        //**************************************************************************************************
         if(readString.indexOf('c') >0) {
           if(n < 0 && topLimit == HIGH) {
             digitalWrite(InB2, HIGH);
@@ -225,7 +237,7 @@ void loop() {
       }
       readString="";
     }
-    else {     
+    else {
       readString += c;
     }
   }
