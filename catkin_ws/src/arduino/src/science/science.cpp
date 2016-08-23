7d915f4b4d3ef4d1262ea82ebbe8f42f438ff34f
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-08-23 13:30:03 -0400

    Refactor auger velocity naming

--- catkin_ws/src/arduino/src/science/science.cpp
+++ catkin_ws/src/arduino/src/science/science.cpp
@@ -5,7 +5,6 @@
 #include <Wire.h>
 #include <ros.h>
 #include "Barometer.h"
-#include "PhSensor.h"
 #include <arduino/sensor.h>
 #include <arduino/LimitSwitchScience.h>
 #include "Humidity.h"
@@ -19,20 +18,20 @@
 
 unsigned long switch_pub_schedule = 0;
 void sensorServiceCallback(const arduino::sensor::Request &request, arduino::sensor::Response &response);
-void handle_auger_velocity(const std_msgs::Int16 & message);
-void handle_auger_position(const std_msgs::Int16 & message);
+void handle_auger_angular_velocity(const std_msgs::Int16 &message);
+void handle_auger_vertical_velocity(const std_msgs::Int16 &message);
 
 Servo tempProbServo;
 
 arduino::LimitSwitchScience limitSwitchMsg;
 
-int16_t last_command_auger_velocity = 0;
-int16_t last_command_auger_position = 0;
+int16_t last_command_auger_angular_velocity = 0; // How fast and in what direction to spin the auger
+int16_t last_command_auger_vertical_velocity = 0; // How fast and in what direction to move the auger up or down
 int16_t last_command_gate_position = 0;
 int16_t last_command_prob_position = 0;
 
-ros::Subscriber<std_msgs::Int16> augerVelocitySub("auger_velocity", &handle_auger_velocity);
-ros::Subscriber<std_msgs::Int16> augerPositionSub("auger_position", &handle_auger_position);
+ros::Subscriber<std_msgs::Int16> augerVelocitySub("auger_velocity", &handle_auger_angular_velocity);
+ros::Subscriber<std_msgs::Int16> augerPositionSub("auger_position", &handle_auger_vertical_velocity);
 ros::Publisher limitSwitchPub("limit_switch", &limitSwitchMsg);
 
 Barometer * barometer;
@@ -41,51 +40,54 @@ WindSensor * windSensor;
 ros::NodeHandle nodeHandle;
 ros::ServiceServer<arduino::sensor::Request, arduino::sensor::Response> sensorService ("sensor_server", &sensorServiceCallback);
 
-void setup()
-{
-    pinMode(PIN_AUGER_POSITION_INA, OUTPUT);
-    pinMode(PIN_AUGER_POSITION_INB, OUTPUT);
-    pinMode(PIN_AUGER_POSITION_PWM, OUTPUT);
 
-    pinMode(PIN_AUGER_VELOCITY_INA, OUTPUT);
-    pinMode(PIN_AUGER_VELOCITY_INB, OUTPUT);
-    pinMode(PIN_AUGER_VELOCITY_PWM, OUTPUT);
+/**
+ * Arduino setup
+ */
+void setup(){
+    // Up / Down auger velocity pins
+    pinMode(PIN_AUGER_VERTICAL_VELOCITY_INA, OUTPUT);
+    pinMode(PIN_AUGER_VERTICAL_VELOCITY_INB, OUTPUT);
+    pinMode(PIN_AUGER_VERTICAL_VELOCITY_PWM, OUTPUT);
 
-    // Need to change title (need to remove prob and gate)
+    // Spin velocity pins for the auger
+    pinMode(PIN_AUGER_ANGULAR_VELOCITY_INA, OUTPUT);
+    pinMode(PIN_AUGER_ANGULAR_VELOCITY_INB, OUTPUT);
+    pinMode(PIN_AUGER_ANGULAR_VELOCITY_PWM, OUTPUT);
+
+    // Limit switches for the auger
     pinMode(PIN_LIMIT_SWITCH_UP, INPUT_PULLUP);
     pinMode(PIN_LIMIT_SWITCH_DOWN, INPUT_PULLUP);
 
     tempProbServo.attach(PIN_TEMP_PROB_POSITION);
+
+    // Initialize ROS interactions
     nodeHandle.initNode();
     nodeHandle.advertiseService(sensorService);
     nodeHandle.subscribe(augerVelocitySub);
     nodeHandle.subscribe(augerPositionSub);
     nodeHandle.advertise(limitSwitchPub);
 
+    // Initialize interfaces for Science sensors
     barometer = new Barometer;
     windSensor = new WindSensor;
 }
 
-void loop ()
-{
+/**
+ * Arduino loop
+ */
+void loop (){
     nodeHandle.spinOnce();
-    if (!digitalRead(PIN_LIMIT_SWITCH_UP) && last_command_auger_position > 0){
-        analogWrite(PIN_AUGER_POSITION_PWM, 0);
+
+    // We need to check the limit switches explicitly in every loop
+    if (!digitalRead(PIN_LIMIT_SWITCH_UP) && last_command_auger_vertical_velocity > 0){
+        analogWrite(PIN_AUGER_VERTICAL_VELOCITY_PWM, 0);
     }
-    if (!digitalRead(PIN_LIMIT_SWITCH_DOWN) && last_command_auger_position < 0) {
-        analogWrite(PIN_AUGER_POSITION_PWM, 0);
+    if (!digitalRead(PIN_LIMIT_SWITCH_DOWN) && last_command_auger_vertical_velocity < 0) {
+        analogWrite(PIN_AUGER_VERTICAL_VELOCITY_PWM, 0);
     }
 
-    // These are being left as examples for how to write the other servo code
-    // as soon as that is complete the PIN_LIMIT_SWITCH_GATE and
-    // PIN_LIMIT_SWITCH_PROB conditionals can (should) be removed.
-    if (!digitalRead(PIN_LIMIT_SWITCH_GATE) && last_command_gate_position > 0) {
-        analogWrite(PIN_GATE_POSITION_PWM, 0);
-    }
-    if (!digitalRead(PIN_LIMIT_SWITCH_PROB) && last_command_prob_position < 0) {
-        tempProbServo.writeMicroseconds(1500);
-    }
-
+    // Publish limit switch details
     if( switch_pub_schedule < millis() ){
         switch_pub_schedule += SWITCH_PUB_TIMEOUT;
         limitSwitchMsg.limit_switch_up = digitalRead(PIN_LIMIT_SWITCH_UP);
@@ -94,7 +96,7 @@ void loop ()
     }
 }
 
-void sensorServiceCallback(const arduino::sensor::Request &request, arduino::sensor::Response &response) {
+void sensorServiceCallback(const arduino::sensor::Request &request, arduino::sensor::Response &response){
     response.pressure = barometer->get_pressure()/5.5238471703;
     response.altitude = barometer->get_altitude();
     response.ambiant_temperature = barometer->get_ambient_temperature();
@@ -102,33 +104,34 @@ void sensorServiceCallback(const arduino::sensor::Request &request, arduino::sen
     response.wind_speed = windSensor->get_wind_speed();
 }
 
-void handle_auger_velocity(const std_msgs::Int16 & message){
-    last_command_auger_velocity = message.data;
-    if(last_command_auger_velocity > 0 ){
-        digitalWrite(PIN_AUGER_VELOCITY_INA, HIGH);
-        digitalWrite(PIN_AUGER_VELOCITY_INB, LOW);
-        analogWrite(PIN_AUGER_VELOCITY_PWM, min(last_command_auger_velocity,255));
-    } else if (last_command_auger_velocity < 0){
-        digitalWrite(PIN_AUGER_VELOCITY_INA, LOW);
-        digitalWrite(PIN_AUGER_VELOCITY_INB, HIGH);
-        analogWrite(PIN_AUGER_VELOCITY_PWM, min(abs(last_command_auger_velocity),255));
+void handle_auger_angular_velocity(const std_msgs::Int16 &message){
+    last_command_auger_angular_velocity = message.data;
+    if(last_command_auger_angular_velocity > 0 ){
+        digitalWrite(PIN_AUGER_ANGULAR_VELOCITY_INA, HIGH);
+        digitalWrite(PIN_AUGER_ANGULAR_VELOCITY_INB, LOW);
+        analogWrite(PIN_AUGER_ANGULAR_VELOCITY_PWM, min(last_command_auger_angular_velocity,255));
+    } else if (last_command_auger_angular_velocity < 0){
+        digitalWrite(PIN_AUGER_ANGULAR_VELOCITY_INA, LOW);
+        digitalWrite(PIN_AUGER_ANGULAR_VELOCITY_INB, HIGH);
+        analogWrite(PIN_AUGER_ANGULAR_VELOCITY_PWM, min(abs(last_command_auger_angular_velocity),255));
     } else {
-        analogWrite(PIN_AUGER_VELOCITY_PWM, 0);
+        analogWrite(PIN_AUGER_ANGULAR_VELOCITY_PWM, 0);
     }
 }
 
-void handle_auger_position(const std_msgs::Int16 & message){
-    last_command_auger_position = message.data;
-    if(digitalRead(PIN_LIMIT_SWITCH_UP) && last_command_auger_position > 0 ){
-        digitalWrite(PIN_AUGER_POSITION_INA, HIGH);
-        digitalWrite(PIN_AUGER_POSITION_INB, LOW);
-        analogWrite(PIN_AUGER_POSITION_PWM, min(last_command_auger_position,255));
-    } else if (last_command_auger_position < 0){
-        digitalWrite(PIN_AUGER_POSITION_INA, LOW);
-        digitalWrite(PIN_AUGER_POSITION_INB, HIGH);
-        analogWrite(PIN_AUGER_POSITION_PWM, min(abs(last_command_auger_position),255));
+void handle_auger_vertical_velocity(const std_msgs::Int16 &message){
+    last_command_auger_vertical_velocity = message.data;
+    if(digitalRead(PIN_LIMIT_SWITCH_UP) && last_command_auger_vertical_velocity > 0 ){
+        digitalWrite(PIN_AUGER_VERTICAL_VELOCITY_INA, HIGH);
+        digitalWrite(PIN_AUGER_VERTICAL_VELOCITY_INB, LOW);
+        analogWrite(PIN_AUGER_VERTICAL_VELOCITY_PWM, min(last_command_auger_vertical_velocity,255));
+    } else if (last_command_auger_vertical_velocity < 0){
+        digitalWrite(PIN_AUGER_VERTICAL_VELOCITY_INA, LOW);
+        digitalWrite(PIN_AUGER_VERTICAL_VELOCITY_INB, HIGH);
+        analogWrite(PIN_AUGER_VERTICAL_VELOCITY_PWM, min(abs(last_command_auger_vertical_velocity),255));
     } else {
-        analogWrite(PIN_AUGER_POSITION_PWM, 0);
+        // If there is an unwanted condition, such as a limit switch being triggered
+        analogWrite(PIN_AUGER_VERTICAL_VELOCITY_PWM, 0);
     }
 }
 
