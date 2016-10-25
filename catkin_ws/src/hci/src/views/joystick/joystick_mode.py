acaaed09fc654eb2af8922417adc146aad2bcd0c
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2016-10-25 17:46:46 -0400

    Document the joystick selection widget

--- catkin_ws/src/hci/src/views/joystick/joystick_mode.py
+++ catkin_ws/src/hci/src/views/joystick/joystick_mode.py
@@ -1,3 +1,7 @@
+"""!@brief Mode selection widget
+
+"""
+
 import sys
 from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication
@@ -7,21 +11,49 @@ from PyQt5.QtWidgets import QWidget
 
 
 class JoystickMode(QWidget):
+    """!@brief Mode selection widget for joystick mode selection
+
+    Signal should be handled by the JoystickController to enable certain
+    JoystickBase to acquire inputs
+
+    """
+
+    ## Signal emitted when someone select a new mode is selected
     changeMode = pyqtSignal(str)
 
     def __init__(self, parent=None):
+        """!@brief Constructor creates the layout and widget list
+
+        Will wait to receive controllers to register
+
+        @param self Python object pointer
+        @param parent Qt object hierarchy
+        """
         super(JoystickMode, self).__init__(parent)
 
-        self.button_list = []
-        self.layout = QHBoxLayout()
-        self.setLayout(self.layout)
+        self._button_list = []
+        self._layout = QHBoxLayout()
+        self.setLayout(self._layout)
 
     @pyqtSlot(str)
     def addMode(self, name):
+        """!@brief Slot to add joystick modes to the display.
+
+        Every mode is a different button on screen.
+
+        @param self Python object pointer
+        @param name String with the name of the mode, will be the display text
+        on the button
+        """
         button=QPushButton(name, self)
-        self.layout.addWidget(button)
+        self._layout.addWidget(button)
 
         def button_callback():
+            """!@brief Callback function created for every new button.
+
+            This function will emit the changeMode signal with the proper
+            button name when clicked
+            """
             name = button.text()
             self.changeMode.emit(name)
 
