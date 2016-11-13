c3554a54488ed97ed37a314f26b32c6849ba4f1e
Kael Du <agains1595@gmail.com>
2016-11-12 18:33:48 -0800

    Add current position and heading of robot

--- catkin_ws/src/hci/src/views/main_view.py
+++ catkin_ws/src/hci/src/views/main_view.py
@@ -3,17 +3,18 @@ from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
 
 from views.drive.drive_view import DriveView
+from views.map.pose_display import PoseDisplay
 from views.drive.wheel_status import WheelStatus
 from views.joystick.joystick_mode import JoystickMode
 from views.navigation.navigation_view import NavigationView
 
 
 class MainView(QWidget):
+
     def __init__(self, parent=None):
         super(MainView, self).__init__(parent)
         h1 = QHBoxLayout()
         v1 = QVBoxLayout()
-
 
         self.joystick_mode_widget = JoystickMode(self)
         self.drive_view = DriveView(self)
@@ -25,3 +26,9 @@ class MainView(QWidget):
         v1.addWidget(self.navigation_view)
 
         self.setLayout(v1)
+        self.pose_display = PoseDisplay(self)
+        self.layout.addWidget(self.joystick_mode_widget)
+        self.layout.addWidget(self.drive_view)
+        self.layout.addWidget(self.pose_display)
+
+        self.setLayout(self.layout)
