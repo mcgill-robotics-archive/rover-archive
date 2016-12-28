75ba3d7f904b29de97081f6395a04fe2a6bb8a42
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-12-28 10:53:34 -0500

    Update the setup script to install from repos

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Initialize rover repository into working state
+
+
+# Fail on first error
+set -e
+
+# Colors <3
+header=$(tput setab 1; tput setaf 0)
+question=$(tput bold; tput setaf 4)
+section=$(tput bold; tput setaf 2)
+warning=$(tput bold; tput setaf 1)
+reset=$(tput sgr0)
+
+# Welcome the user
+echo "${header} Welcome to the Rover init script." 
+echo "This script automates initial dependency resolution and building of the"
+echo "workspace. It also optionally handles the setup of MoveIt! You will" 
+echo "likely be prompted several times within the first minute.${reset}"
+echo
+
+# Ask for sudo privileges if not already availlable.
+sudo echo "Checking for sudo priviledges..." || sudo -K
+sudo echo "Sudo priviledges are availlable!" || exit 1
+echo
+
+# Update from ubuntu repositories
+echo "Updating and upgrading using apt..."
+sudo apt-get update
+echo
+
+# Update repository
+echo "Pulling latest changes..."
+git pull && git submodule update --init --recursive
+echo
+
+# convex_decompositon
+echo "Installing convex_decomposition..."
+sudo apt-get install -y ros-kinetic-convex-decomposition
+echo
+
+# ROS dependency update
+echo "Installing ros dependencies for catkin workspace..."
+rosdep update
+rosdep install --from-paths ./catkin_ws --ignore-src --skip-keys=rover_msgs \
+    --rosdistro kinetic
+echo
+
+# Set up MoveIt!
+echo "Installing MoveIt! from repositories..."
+sudo apt-get install ros-kinetic-moveit
+echo
+
+echo "All done with Rover setup!"
