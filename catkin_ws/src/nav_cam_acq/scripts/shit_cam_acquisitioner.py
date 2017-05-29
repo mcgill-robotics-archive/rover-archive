26bed486e68ea7f812f8084ff9342e179a975171
reivilo444 <o.lamarre7@gmail.com>
2017-05-29 12:16:58 -0600

    Created shitty camera acquisition node using opencv.

---
+++ catkin_ws/src/nav_cam_acq/scripts/shit_cam_acquisitioner.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import cv2
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def acquire(cap, device_id):
+
+    bridge = CvBridge()
+    comp_img = CompressedImage()
+
+    _, frame = cap.read()
+
+    try:
+        ros_img = bridge.cv2_to_imgmsg(frame, "bgr8")
+    except CvBridgeError as err:
+        print(err)
+
+    comp_img.data = np.array(cv2.imencode(".jpg", frame)[1]).tostring()
+    comp_img.header = ros_img.header
+    comp_img.format = "jpeg"
+
+    return comp_img
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('navcam_acquisitioner', anonymous=False)
+    # img_pub = rospy.Publisher("camera", Image, queue_size=10)
+    comp_pub = rospy.Publisher("camera/compressed", CompressedImage, queue_size=10)
+    device_id = rospy.get_param("device_id", 0)
+    capture = cv2.VideoCapture(device_id)
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        compressed = acquire(capture, device_id)
+        # img_pub.publish(image)
+        comp_pub.publish(compressed)
+
+        rate.sleep()
