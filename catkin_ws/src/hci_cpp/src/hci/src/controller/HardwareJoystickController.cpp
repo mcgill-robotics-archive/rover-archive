613c432592343d6ce58b3ebbce2f9b1963835e6b
McGill Robotics <dev@mcgillrobotics.com>
2018-01-13 13:58:34 -0500

    Added a second while loop in process to fix lag

--- catkin_ws/src/hci_cpp/src/hci/src/controller/HardwareJoystickController.cpp
+++ catkin_ws/src/hci_cpp/src/hci/src/controller/HardwareJoystickController.cpp
@@ -5,6 +5,8 @@
 
 #include "HardwareJoystickController.h"
 #include <ros/ros.h>
+//the bleow line was added
+#include <unistd.h>
 
 HardwareJoystickController::HardwareJoystickController() :
         mJoystick("/dev/input/js0")
@@ -23,25 +25,26 @@ void HardwareJoystickController::process() {
     {
         // poll joystick for new info
         JoystickEvent event;
-        mJoystick.sample(&event);
-        if (event.isButton())
-        {
-            mData.buttons[event.number] = event.value;
-        }
+	while (mJoystick.sample(&event)) {//added a second while loop
+        	if (event.isButton())
+        	{
+            	mData.buttons[event.number] = event.value;
+        	}
 
-        else if (event.isAxis())
-        {
-            if (event.number == 0)
-                mData.a1 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
-            else if (event.number == 1)
-                mData.a2 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
-            else if (event.number == 2)
-                mData.a3 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
-            else if (event.number == 3)
-                mData.a4 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
-        }
+        	else if (event.isAxis())
+        	{
+            	if (event.number == 0)
+                	mData.a1 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
+           	else if (event.number == 1)
+                	mData.a2 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
+           	else if (event.number == 2)
+                	mData.a3 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
+            	else if (event.number == 3)
+                	mData.a4 = (float) event.value / (float) JoystickEvent::MAX_AXES_VALUE;
+        	}
 
-        emit joystickDataUpdated(mData);
+        	emit joystickDataUpdated(mData);
+	}//added
         r.sleep();
     }
 
