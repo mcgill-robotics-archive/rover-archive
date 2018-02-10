7e79490651b50d102daae79d6605d46b29f2d0a2
SandraDeng <sandradeng1998@163.com>
2018-02-10 17:28:56 -0500

    point cloud

--- catkin_ws/src/ahrs/src/tf_broadcaster.py
+++ catkin_ws/src/ahrs/src/tf_broadcaster.py
@@ -5,12 +5,18 @@ import rospy
 from tf.transformations import quaternion_multiply
 import tf
 import math
+import std_msgs.msg
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import PointCloud
 from ahrs.msg import AhrsStdMsg
 from ahrs.srv import *
 
 
+
 class AhrsTfBroadcaster:
+	
     def __init__(self):
+	
         rospy.init_node('tf_broadcaster')
         self.sub = rospy.Subscriber("ahrs_status",
                                     AhrsStdMsg, self.handle_ahrs_msg)
@@ -19,6 +25,30 @@ class AhrsTfBroadcaster:
                                          CenterWorldFrame, self.handle_service)
 
         self.br = tf.TransformBroadcaster()
+
+	#header
+	self.h = std_msgs.msg.Header()
+	self.h.stamp = rospy.Time.now()
+	self.h.frame_id = "base_link"
+	
+	self.ahrsPublisher = rospy.Publisher('AHRS', PointCloud, queue_size=10)
+	
+	self.point_cloud = PointCloud()
+	self.point_cloud.header = self.h
+	
+	self.start_time = rospy.Time.now()
+
+	#self.drift_x = -0.00001
+	#self.drift_y = -0.000295
+	#self.x =0.06
+	#self.y =0
+	self.drift_x=0
+	self.drift_y=0
+	self.x=0
+	self.y=0
+	self.start_time = rospy.Time.now() 
+	self.time = rospy.Time.now()
+	self.positionList = []
         self.center_position = (0, 0, 0)
         self.last_tf = None
         self.first = True
@@ -44,6 +74,7 @@ class AhrsTfBroadcaster:
                 message.pose.pose.position.x,
                 message.pose.pose.position.y,
                 message.pose.pose.position.z)
+	    self.positionList.append(self.center_position)
             self.first = False
 
         quat = (
@@ -54,11 +85,18 @@ class AhrsTfBroadcaster:
 
         euler = tf.transformations.euler_from_quaternion(quat)
         euler_fixed = (euler[0], -1 * euler[1], -1 * euler[2])
+	
+	if self.last_tf is not None:
+		self.x += (message.pose.pose.position.x - self.last_tf.x)- self.drift_x
+		self.y += (message.pose.pose.position.y - self.last_tf.y)- (self.drift_y )
+	else: 
+		self.x += (message.pose.pose.position.x - self.drift_x)
+		self.y += (message.pose.pose.position.y - self.drift_y)
 
-        self.br.sendTransform(((message.pose.pose.position.x - self.center_position[0]) / 10, (message.pose.pose.position.y - self.center_position[1]) / 10 ,0), (0, 0, 0, 1),
+        self.br.sendTransform(( self.x, self.y, 0), (0, 0, 0, 1),
                               rospy.Time.now(),
                               "ahrs_position", "base_link")
-
+       
         # This adjustment makes the local frame for the ahrs conform with
         # the (forward, left, up) standard for local robot coordinate frames.
         # The AHRS is by default (forward, right, down).
@@ -73,12 +111,22 @@ class AhrsTfBroadcaster:
         # The AHRS is by default (north, east, down).
         sqrt2_2 = math.sqrt(2) / 2.0
         global_orientation_adjustment = (0, sqrt2_2, -sqrt2_2, 0)
+
         self.br.sendTransform((0, 0, 0), quat,
                               rospy.Time.now(), "global_ahrs_orientation",
                               "ahrs_position")
-
+	
         self.last_tf = message.pose.pose.position
-
+	#record position every 5s
+	if (rospy.Time.now()-rospy.Duration(5)) >= self.time :
+		self.point = Point()
+		self.point.x = self.x
+		self.point.y = self.y
+		self.point.z = 0
+		self.point_cloud.points.append(self.point)
+		self.time = rospy.Time.now()
+		
+	self.ahrsPublisher.publish(self.point_cloud)
 
 if __name__ == "__main__":
     tf_broadcaster = AhrsTfBroadcaster()
