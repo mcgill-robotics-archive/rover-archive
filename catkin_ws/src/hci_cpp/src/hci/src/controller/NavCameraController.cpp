8a9c812ef16a4f3d2435b880d75e5d53f155b598
McGill Robotics <dev@mcgillrobotics.com>
2018-02-17 13:00:46 -0500

    updated cameras and science tab implemetation

--- catkin_ws/src/hci_cpp/src/hci/src/controller/NavCameraController.cpp
+++ catkin_ws/src/hci_cpp/src/hci/src/controller/NavCameraController.cpp
@@ -9,19 +9,19 @@
 NavCameraController::NavCameraController(NavCameraView* navCameraView) : navCamViewPtr(navCameraView)
 {
 //    topFeedPtr.reset(new rimstreamer::NyanVideoFeed(rimstreamer::CAT));
-    topFeedPtr.reset(new rimstreamer::AxisF34VideoFeed("192.168.3.100", rimstreamer::F34_CAMERA_1, rimstreamer::F34_854x480, rimstreamer::Landscape));
+    //topFeedPtr.reset(new rimstreamer::AxisF34VideoFeed("192.168.3.100", rimstreamer::F34_CAMERA_1, rimstreamer::F34_854x480, rimstreamer::Landscape));
 //    leftFeedPtr.reset(new rimstreamer::NyanVideoFeed(rimstreamer::DOG));
-    leftFeedPtr.reset(new rimstreamer::AxisF34VideoFeed("192.168.3.100", rimstreamer::F34_CAMERA_2, rimstreamer::F34_480x360, rimstreamer::Portrait));
+    //leftFeedPtr.reset(new rimstreamer::AxisF34VideoFeed("192.168.3.100", rimstreamer::F34_CAMERA_2, rimstreamer::F34_480x360, rimstreamer::Portrait));
 //    rightFeedPrt.reset(new rimstreamer::NyanVideoFeed(rimstreamer::DOG));
-    rightFeedPrt.reset(new rimstreamer::AxisF34VideoFeed("192.168.3.100", rimstreamer::F34_CAMERA_3, rimstreamer::F34_480x360, rimstreamer::Portrait));
+    //rightFeedPrt.reset(new rimstreamer::AxisF34VideoFeed("192.168.3.100", rimstreamer::F34_CAMERA_3, rimstreamer::F34_480x360, rimstreamer::Portrait));
 
-    navCamViewPtr->setVideoFeed(topFeedPtr, NavCameraView::TOP);
-    navCamViewPtr->setVideoFeed(leftFeedPtr, NavCameraView::LEFT);
-    navCamViewPtr->setVideoFeed(rightFeedPrt, NavCameraView::RIGHT);
+   // navCamViewPtr->setVideoFeed(topFeedPtr, NavCameraView::TOP);
+   // navCamViewPtr->setVideoFeed(leftFeedPtr, NavCameraView::LEFT);
+   // navCamViewPtr->setVideoFeed(rightFeedPrt, NavCameraView::RIGHT);
 
-    topFeedPtr->play();
-    leftFeedPtr->play();
-    rightFeedPrt->play();
+   // topFeedPtr->play();
+   // leftFeedPtr->play();
+   // rightFeedPrt->play();
 
 //    rightFeedPrt->takeSnapshot("./file.jpeg");
 }
