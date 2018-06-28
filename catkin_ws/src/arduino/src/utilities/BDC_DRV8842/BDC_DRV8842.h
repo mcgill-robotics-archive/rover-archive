751eea66912b7f24cca9f0866a0cfd288b5ed9e8
Alexandre Coulombe <32465816+acoulombe@users.noreply.github.com>
2018-06-27 22:14:34 -0400

    Update Arduino Librairies and Comment

--- catkin_ws/src/arduino/src/utilities/BDC_DRV8842/BDC_DRV8842.h
+++ catkin_ws/src/arduino/src/utilities/BDC_DRV8842/BDC_DRV8842.h
@@ -1,3 +1,18 @@
+//****************************************************************//
+// Electrical Division, Mars Rover Team of McGill Robotics
+// Authors: Alexandre Coulombe, Chelsea Myers-Colet, Jeslan Rajendram 
+// Winter 2018
+// version: 1.0
+//  
+// BDC board code used in the Mars Rover's
+// Arm system, Science system, and Drive system
+// Functionality:
+//          - DRV8842 motor control
+//          - DRV8842 fault monitoring
+//          - DRV8842 IC reset
+//          - CSD16340 MOSFET control
+//****************************************************************//
+
 #ifndef BDC_h
 #define BDC_h
 
@@ -5,16 +20,19 @@
 
 class BDC{
   public:
-    BDC(uint8_t In1, uint8_t In2, uint8_t nRST, uint8_t nFLT);
+    BDC(int In1, int In2, int nRST, int nFLT, int BRK);
     void PWM(int PWM_val);
     void RST();
-	int FLT();
+    int FLT();
+    void BRK(boolean active);
+    boolean BRK_activity();
   
   private: 
-    uint8_t _In1;
-	uint8_t _In2;
-    uint8_t _nRST;
-	uint8_t _nFLT;
+    int _In1;
+    int _In2;
+    int _nRST;
+    int _nFLT;
+    int _BRK;
 };
 
 #endif
